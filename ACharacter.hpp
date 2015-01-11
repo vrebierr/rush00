00e9178c678d1cf76666cc470f5425c7b0cbaf21
Cyprien Hery <chery@Cypriens-MacBook-Pro.local>
2015-01-11 20:06:41 +0100

    Implementation class Missile | Corrections autres class

--- ACharacter.hpp
+++ ACharacter.hpp
@@ -12,8 +12,15 @@ public:
 	ACharacter &		operator=(Entity const &);
 	virtual ~ACharacter();
 
+	typedef enum {
+		UP,
+		DOWN,
+		RIGHT,
+		LEFT
+	}		e_dir;
+
 	virtual void		attack() = 0;
-	virtual void		move(enum dir) = 0;
+	virtual void		move(e_dir) = 0;
 };
 
 #endif
